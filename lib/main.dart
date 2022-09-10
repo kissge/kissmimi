3ee1a8d84c5ead4124de030d741f3e420955749a
Yusuke Kido <mail@yusuk.eki.do>
2022-09-10 18:44:52 +0900

    proof of concept

---
+++ lib/main.dart
@@ -0,0 +1,271 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:just_waveform/just_waveform.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? inputFile;
+  double? progress;
+  Stream<WaveformProgress>? progressStream;
+  Waveform? waveform;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   print(Directory.current);
+
+  //   final progressStream = JustWaveform.extract(
+  //     audioInFile: File('./drums.wav'),
+  //     waveOutFile: File('./out.wave'),
+  //     zoom: const WaveformZoom.pixelsPerSecond(100),
+  //   );
+  //   progressStream.listen((waveformProgress) {
+  //     print('Progress: %${(100 * waveformProgress.progress).toInt()}');
+  //     if (waveformProgress.waveform != null) {
+  //       print(waveformProgress.waveform!.data.length);
+  //     }
+  //   });
+  // }
+
+  void _incrementCounter() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      setState(() {
+        inputFile = File(result.files.single.path!);
+      });
+
+      progressStream = JustWaveform.extract(
+        audioInFile: inputFile!,
+        waveOutFile: File('./out.wave'),
+        zoom: const WaveformZoom.pixelsPerSecond(200),
+      );
+      progressStream!.listen((waveformProgress) {
+        setState(() {
+          progress = waveformProgress.progress;
+
+          if (waveformProgress.waveform != null) {
+            waveform = waveformProgress.waveform;
+          }
+        });
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: waveform == null
+            ? Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('inputFile = $inputFile'),
+                  Text('progress = $progress'),
+                  Text('waveform.length = ${waveform?.length}'),
+                ],
+              )
+            : Container(
+                child: AudioWaveformWidget(
+                waveform: waveform!,
+                start: Duration.zero,
+                duration: waveform!.duration,
+                pixelsPerStep: 1.0,
+                strokeWidth: 1.0,
+              )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AudioWaveformWidget extends StatefulWidget {
+  final Color waveColor;
+  final double scale;
+  final double strokeWidth;
+  final double pixelsPerStep;
+  final Waveform waveform;
+  final Duration start;
+  final Duration duration;
+
+  const AudioWaveformWidget({
+    Key? key,
+    required this.waveform,
+    required this.start,
+    required this.duration,
+    this.waveColor = Colors.blue,
+    this.scale = 1.0,
+    this.strokeWidth = 5.0,
+    this.pixelsPerStep = 8.0,
+  }) : super(key: key);
+
+  @override
+  _AudioWaveformState createState() => _AudioWaveformState();
+}
+
+class _AudioWaveformState extends State<AudioWaveformWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: CustomPaint(
+        size: Size.infinite,
+        painter: AudioWaveformPainter(
+          waveColor: widget.waveColor,
+          waveform: widget.waveform,
+          start: widget.start,
+          duration: widget.duration,
+          scale: widget.scale,
+          strokeWidth: widget.strokeWidth,
+          pixelsPerStep: widget.pixelsPerStep,
+        ),
+      ),
+    );
+  }
+}
+
+class AudioWaveformPainter extends CustomPainter {
+  final double scale;
+  final double strokeWidth;
+  final double pixelsPerStep;
+  final Paint wavePaint;
+  final Waveform waveform;
+  final Duration start;
+  final Duration duration;
+
+  AudioWaveformPainter({
+    required this.waveform,
+    required this.start,
+    required this.duration,
+    Color waveColor = Colors.blue,
+    this.scale = 1.0,
+    this.strokeWidth = 5.0,
+    this.pixelsPerStep = 8.0,
+  }) : wavePaint = Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = strokeWidth
+          ..strokeCap = StrokeCap.round
+          ..color = waveColor;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (duration == Duration.zero) return;
+
+    double width = size.width;
+    double height = size.height;
+
+    final waveformPixelsPerWindow = waveform.positionToPixel(duration).toInt();
+    final waveformPixelsPerDevicePixel = waveformPixelsPerWindow / width;
+    final waveformPixelsPerStep = waveformPixelsPerDevicePixel * pixelsPerStep;
+    final sampleOffset = waveform.positionToPixel(start);
+    final sampleStart = -sampleOffset % waveformPixelsPerStep;
+    for (var i = sampleStart.toDouble();
+        i <= waveformPixelsPerWindow + 1.0;
+        i += waveformPixelsPerStep) {
+      final sampleIdx = (sampleOffset + i).toInt();
+      final x = i / waveformPixelsPerDevicePixel;
+      final minY = normalise(waveform.getPixelMin(sampleIdx), height);
+      final maxY = normalise(waveform.getPixelMax(sampleIdx), height);
+      canvas.drawLine(
+        Offset(x + strokeWidth / 2, max(strokeWidth * 0.75, minY)),
+        Offset(x + strokeWidth / 2, min(height - strokeWidth * 0.75, maxY)),
+        wavePaint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant AudioWaveformPainter oldDelegate) {
+    return false;
+  }
+
+  double normalise(int s, double height) {
+    if (waveform.flags == 0) {
+      final y = 32768 + (scale * s).clamp(-32768.0, 32767.0).toDouble();
+      return height - 1 - y * height / 65536;
+    } else {
+      final y = 128 + (scale * s).clamp(-128.0, 127.0).toDouble();
+      return height - 1 - y * height / 256;
+    }
+  }
+}
